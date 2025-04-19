3092a8e2e2f844582123ba9493eba5dce99877f4
Omi354 <n.izumi@aby-tech.co.jp>
2025-04-19 15:52:58 +0900

    chore: 日本語化しました

--- config/application.rb
+++ config/application.rb
@@ -38,5 +38,9 @@ module Laboratory
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    # 日本語化する
+    config.i18n.default_locale = :ja
+    config.time_zone = "Tokyo"
   end
 end
